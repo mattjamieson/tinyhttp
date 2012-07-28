f558467ed653dfdd7edfe143d4d3d103a24d10ce
Matt Jamieson <mattjamieson@gmail.com>
2012-07-28 10:39:32 +0100

    Added RedirectResponse, tests

---
+++ tests/TinyHttp.Tests/NotFoundResponseTest.cs
@@ -0,0 +1,24 @@
+﻿namespace TinyHttp.Tests
+{
+    using System.Net;
+    using NUnit.Framework;
+
+    public class NotFoundResponseTest
+    {
+        [Test]
+        public void NotFoundResponse()
+        {
+            var response = new NotFoundResponse("application/json");
+            Assert.AreEqual("application/json", response.ContentType);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Test]
+        public void DefaultNotFoundResponse()
+        {
+            var response = new NotFoundResponse();
+            Assert.AreEqual("text/html", response.ContentType);
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ tests/TinyHttp.Tests/RedirectResponseTest.cs
@@ -0,0 +1,58 @@
+﻿namespace TinyHttp.Tests
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using NUnit.Framework;
+
+    public class RedirectResponseTest
+    {
+        [Test]
+        public void PermanentRedirect()
+        {
+            var response = new RedirectResponse("/", RedirectResponse.RedirectType.Permanent);
+            Assert.AreEqual("/", response.Headers["Location"]);
+            Assert.AreEqual(String.Empty, BodyAsString(response));
+            Assert.AreEqual(HttpStatusCode.MovedPermanently, response.StatusCode);
+        }
+
+        [Test]
+        public void TemporaryRedirect()
+        {
+            var response = new RedirectResponse("/", RedirectResponse.RedirectType.Temporary);
+            Assert.AreEqual("/", response.Headers["Location"]);
+            Assert.AreEqual(String.Empty, BodyAsString(response));
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
+        }
+
+        [Test]
+        public void SeeOtherRedirect()
+        {
+            var response = new RedirectResponse("/", RedirectResponse.RedirectType.SeeOther);
+            Assert.AreEqual("/", response.Headers["Location"]);
+            Assert.AreEqual(String.Empty, BodyAsString(response));
+            Assert.AreEqual(HttpStatusCode.SeeOther, response.StatusCode);
+        }
+
+        [Test]
+        public void DefaultRedirect()
+        {
+            var response = new RedirectResponse("/");
+            Assert.AreEqual("/", response.Headers["Location"]);
+            Assert.AreEqual(String.Empty, BodyAsString(response));
+            Assert.AreEqual(HttpStatusCode.SeeOther, response.StatusCode);
+        }
+
+        private static string BodyAsString(Response response)
+        {
+            string body;
+            using (var stream = new MemoryStream())
+            {
+                response.Body.Invoke(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                body = new StreamReader(stream).ReadToEnd();
+            }
+            return body;
+        }
+    }
+}
